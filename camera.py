2c761347eb707dbe63906ea1d6167aa426e05d1b
nguyenric <nguyen.ric@northeastern.edu>
2026-04-26 17:06:46 -0400

    added mediapipe for hand landmarks, currently only detecting hand coordinates

--- camera.py
+++ camera.py
@@ -1,46 +1,39 @@
-import math
+
 import cv2
-import numpy
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
 
 
 
-cam = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# 1. Set your options
+options = mp.tasks.vision.HandLandmarkerOptions(
+    base_options=mp.tasks.BaseOptions(model_asset_path='hand_landmarker.task'),
+    num_hands=2  # how many hands to detect
+)
 
-# tells the code what type of format to put it as, in this case, a mp4 video, 
-# and then it creates a video writer object that will write the frames to a file 
-# called output.mp4, with a frame rate of 20.0 and the same width and height as the camera feed.
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (frame_width, frame_height))
-runmode = mp.tasks.vision.RunningMode 
-base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+# 2. Create the landmarker using those options
+landmarker = mp.tasks.vision.HandLandmarker.create_from_options(options)
+    
+
 
 # This loop continuously reads frames from the camera feed, 
 # writes them to the output video file, and displays them in a window.
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        break
+    success, img = cap.read()
 
-    out.write(frame)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    converted = mp.Image(mp.ImageFormat.SRGB,img_rgb)
+    result = landmarker.detect(converted)
+    print(result)
+   
+    
 
-    cv2.imshow('Camera', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
 
     
 
-cam.release()
-out.release()
-cv2.destroyAllWindows()
-cam.quit()
 
 
