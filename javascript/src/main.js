da9a5fbd6deec80b0a32f18825c07295358a8d1d
nguyenric <nguyen.ric@northeastern.edu>
2026-07-05 19:55:37 -0400

    implemented Vite template, got camera and hand detection working

--- javascript/src/main.js
+++ javascript/src/main.js
@@ -1,4 +1,4 @@
-const { HandLandmarker, FilesetResolver } = vision
+import { HandLandmarker, FilesetResolver } from '@mediapipe/tasks-vision'
 
 async function main() {
     const visionTasks = await FilesetResolver.forVisionTasks(
@@ -8,21 +8,28 @@ async function main() {
         baseOptions: {
             modelAssetPath: "hand_landmarker.task"
         },
-        numHands: 2
+        numHands: 2,
+        runningMode: "VIDEO"
     });
 
     const video = document.getElementById('video')
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.srcObject = stream
         video.play()
+        setTimeout(() => detectHands(), 1000)
     })
 
     function detectHands() {
-        const results = handLandmarker.detectForVideo(video, Date.now())
-        console.log(results)
-        requestAnimationFrame(detectHands)
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0);
+    
+    const results = handLandmarker.detectForVideo(canvas, Date.now());
+    console.log(results);
+    requestAnimationFrame(detectHands);
     }
-    detectHands()
 }
 
 main()
